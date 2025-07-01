2172e8076e0dfc5ef109237a985035c7d3def22a
amin kafri <aminkafrinewgen@gmail.com>
2025-07-01 22:57:54 +0800

    print the target file's filepath

--- main.zig
+++ main.zig
@@ -1,5 +1,26 @@
 const std = @import("std");
 
-pub fn main() void {
-    std.debug.print("Hello, World\n", .{});
+pub fn main() !void {
+    const path = ".";
+    const needle = "tagit.c";
+    try printDirContents(path, needle);
+}
+
+pub fn printDirContents(path: []const u8, needle: []const u8) !void {
+    var dir = try std.fs.cwd().openDir(path, .{ .iterate = true });
+    defer dir.close();
+
+    var dirIterator = dir.iterate();
+    while (try dirIterator.next()) |dirContent| {
+        if (dirContent.kind == std.fs.File.Kind.directory) {
+            // std.debug.print("{s}/{s}\n", .{ path, dirContent.name });
+            var buf: [1024]u8 = undefined;
+            const foo = try std.fmt.bufPrint(&buf, "{s}/{s}", .{ path, dirContent.name });
+            try printDirContents(foo, needle);
+        } else if (dirContent.kind == std.fs.File.Kind.file) {
+            if (std.mem.eql(u8, dirContent.name, needle)) {
+                std.debug.print("{s}/{s}\n", .{ path, dirContent.name });
+            }
+        }
+    }
 }
